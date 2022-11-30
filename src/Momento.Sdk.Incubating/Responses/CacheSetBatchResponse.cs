f8a0026ff627b8ac83633a7b20d8b95b9ae2f70d
erikatharp <eem@momentohq.com>
2022-11-29 21:02:56 -0800

    chore: add preprocessir directive:

---
+++ src/Momento.Sdk.Incubating/Responses/CacheSetBatchResponse.cs
@@ -0,0 +1,41 @@
+﻿namespace Momento.Sdk.Incubating.Responses;
+
+using Momento.Sdk.Exceptions;
+
+#if USE_UNARY_BATCH
+public abstract class CacheSetBatchResponse
+{
+
+    public class Success : CacheSetBatchResponse { }
+
+    public class Error : CacheSetBatchResponse
+    {
+        private readonly SdkException _error;
+        public Error(SdkException error)
+        {
+            _error = error;
+        }
+
+        public SdkException Exception
+        {
+            get => _error;
+        }
+
+        public MomentoErrorCode ErrorCode
+        {
+            get => _error.ErrorCode;
+        }
+
+        public string Message
+        {
+            get => $"{_error.MessageWrapper}: {_error.Message}";
+        }
+
+        public override string ToString()
+        {
+            return base.ToString() + ": " + Message;
+        }
+    }
+
+}
+#endif
